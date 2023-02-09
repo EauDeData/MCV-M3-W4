30b67101498232a4f2266c298c4187301efd5eef
AtenrevCode <atenrev@gmail.com>
2023-02-09 18:41:06 +0100

    more clear

--- src/tasks.py
+++ src/tasks.py
@@ -445,22 +445,38 @@ def prune_and_train_optuna_model(args, report_file: str = "report_best_model.txt
     # Load weights
     model.load_weights(args.model_weights_file)
 
+    print("\n\n------ Model weights before pruning ------")
+    total_w = 0
+    total_0 = 0
+
     for i, w in enumerate(model.get_weights()):
         print(
-            "\n{} -- Total:{}, Zeros (before pruning): {:.2f}%\n".format(
+            "{} -- Total:{}, Zeros (before pruning): {:.2f}%".format(
                 model.weights[i].name, w.size, np.sum(w == 0) / w.size * 100
             )
         )
+        total_w += w.size
+        total_0 += np.sum(w == 0)
+
+    print(f"\nTotal sparsity: {total_0 / total_w * 100}%\n\n")
 
     # Prune model
     model = prune_model(model, train_set, val_set, config)
 
+    print("\n\n------ Model weights after pruning ------")
+    total_w = 0
+    total_0 = 0
+
     for i, w in enumerate(model.get_weights()):
         print(
-            "\n{} -- Total:{}, Zeros (after pruning): {:.2f}%\n".format(
+            "{} -- Total:{}, Zeros (after pruning): {:.2f}%".format(
                 model.weights[i].name, w.size, np.sum(w == 0) / w.size * 100
             )
         )
+        total_w += w.size
+        total_0 += np.sum(w == 0)
+
+    print(f"\nTotal sparsity: {total_0 / total_w * 100}%\n\n")
 
     # Save model
     os.makedirs("out/models", exist_ok=True)
